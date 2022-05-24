076fab7e2e716ae19c7980701b1d9aa8d6535b81
justinjenecke <justinjenecke@gmail.com>
2022-05-24 21:47:23 +0200

    completes hero - sets up data for projects tab

--- src/app.js
+++ src/app.js
@@ -2,10 +2,64 @@ const App = Vue.createApp({
 
     name : "app",
 
+    //  ==================================== Data ====================================
     data() {
       return {
-        count: 0
+
+        // ===== Hero =====
+        title : "Justin Scott Isaac Jenecke",
+        jobTitle : "Junior Software Developer",
+
+        links : [
+            {
+                type : "Resume",
+                link : "",
+                color : "is-primary"
+            },
+            {
+                type : "GitHub",
+                link : "",
+                color : "is-success"
+            },
+            {
+                type : "LinkedIn",
+                link : "",
+                color : "is-info"
+            }
+        ],
+        // ===== Hero =====
+
+        // ===== Projects =====
+        projectStacks : ["Frontend", "Backend", "Full-Stack"],
+
+        projects : [
+            {
+                name : "project",
+                stack : "none",
+                descript : "lorem",
+                tech : "lrem",
+                previews : [] 
+            }
+        ]
+        // ===== Projects =====
       }
+    },
+
+    //  ==================================== Computed ====================================
+    computed : {
+
+        frontendProjects() {
+            return null
+        },
+        
+        backendProjects() {
+            return null
+        },
+        
+        fullStackProjects() {
+            return null
+        }
+
     }
 
 }).mount('#app')
