b8b197159d44b0e4268ebb6b299cf796e2571a63
justinjenecke <justinjenecke@gmail.com>
2022-05-26 16:44:05 +0200

    fixes typos in about data, makes changes to about template (styling)

--- src/app.js
+++ src/app.js
@@ -46,16 +46,16 @@ const App = Vue.createApp({
         // ===== About =====
         
         about : [
-            "Hi I'm Justin, a Cape Town born Junior software developer. My journey into development began with a program called Game Maker " +
-            "back in primary school. After a few years of hiatus from computers during high school I decided to pick up the keyboard again after " +
-            "thinking about what career I wanted to pursue, which let to me graduating from CPUT with a Diploma in ICT: Application Development.",
+            "Hi, I'm Justin, a Cape Town-born Junior software developer. My development journey began with a program known as " +
+            "'Game Maker' back in primary school. After a few years of hiatus from computers during high school, I decided to pick" +
+            "up the keyboard again after thinking about what career I wanted to pursue, which led to me graduating from CPUT with a Diploma in ICT: Application Development.",
 
-            "My main focus as a developer was always to become a backend engineer, however I have picked a number of frontend skills in past " +
-            "two years so that I could gain more insight into the requirements and workflows of a frontend developer.",
+            "My main focus as a developer is on the backend, however I have picked up several frontend skills in the past two years to " + 
+            "gain more insight into the requirements and workflows of a frontend developer.",
 
-            "I am currently working in an internship role at CodeSpace Academy where I perform website maintence, create sample solutions for course projects " +
-            "and challenges, contribute code walkthroughs for other coaches/lecturers. I am also the current in-house instructor for the company, so I answer." +
-            "most of the student's questions, provide extra coaching when needed, marking, etc."
+            "I am currently working in an internship role at CodeSpace Academy, where I perform website maintenance, create sample solutions " +
+            "for course projects and challenges, and contribute code walkthroughs for other coaches/lecturers. I am also the current in-house " +
+            "instructor for the company, so I answer most of the students' questions, provide extra coaching when needed, marking, etc."
         ],
 
         skills : [
