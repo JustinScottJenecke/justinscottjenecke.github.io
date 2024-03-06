671826a9bf44109035e47378fdf333b85bee5ad9
codespace-justin <justin@codespace.co.za>
2024-03-06 14:53:41 +0200

    dom: creates and links in new script file for handling feature-projects dynamic contnet

---
+++ src/components/3-projects/projects.js
@@ -0,0 +1,16 @@
+// All DOM references for featured projects
+const FP_PREVIEW_IMAGE_ELEMENT = document.querySelector('[data-fp-label="preview-image"]'); 
+const FP_NAME_ELEMENT = document.querySelector('[data-fp-label="name"]'); 
+const FP_CATEGORY_ELEMENT = document.querySelector('[data-fp-label="category"]'); 
+const FP_TECH_STACK_ELEMENT = document.querySelector('[data-fp-label="tech-stack"]'); 
+const FP_ABOUT_ELEMENT = document.querySelector('[data-fp-label="about"]'); 
+const FP_LINKS_ELEMENT = document.querySelector('[data-fp-label="links"]'); 
+
+// checks to ensure DOM references are !null
+console.log(FP_PREVIEW_IMAGE_ELEMENT)
+console.log(FP_PREVIEW_IMAGE_ELEMENT)
+console.log(FP_NAME_ELEMENT)
+console.log(FP_CATEGORY_ELEMENT )
+console.log(FP_TECH_STACK_ELEMENT)
+console.log(FP_ABOUT_ELEMENT)
+console.log(FP_LINKS_ELEMENT)
