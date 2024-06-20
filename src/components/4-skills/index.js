3605ffa3ed66fe893d17e36fde27d47f07ac00b0
justinscottjenecke <justinjenecke@gmail.com>
2024-06-20 11:14:36 +0200

    creates basic template for creating skill card components

---
+++ src/components/4-skills/index.js
@@ -0,0 +1,22 @@
+
+const createSkillCard = (Skill) => {
+
+ const template = `               
+    <li class="border-solid border-4 border-gray-300 p-6" id="${Skill.id}">
+        <div class="card-wrapper w-full aspect-square relative">
+            <div class="card-cover-image w-full aspect-square absolute flex justify-center">
+                <h4 class="absolute flex justify-center">${Skill.name}</h4>
+                <img
+                    src='${Skill.bgImgTag}' />
+            </div>
+            <div class="card-content aspect-square">
+                <ul class="flex flex-col p-3 pt-8">
+                ${
+                    Skill.tools.map(toolItem => `<li class="flex gap-2"><span>${toolItem.tool}</span></li>`)
+                }
+                </ul>
+            </div>
+        </div>
+    </li>`
+}
+
