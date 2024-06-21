7f8587a3ab70f998d815317c74303bd2ed0a42be
justinscottjenecke <justinjenecke@gmail.com>
2024-06-21 23:57:02 +0200

    style: creates grid layout for all project's project cards

--- src/components/3-projects/projects.js
+++ src/components/3-projects/projects.js
@@ -2,14 +2,14 @@
 
 const createSkillCard = (Skill) => {
     return `               
-        <li class="border-solid border-4 border-gray-300 id="${Skill.id}">
-            <div class="card-wrapper w-full aspect-square relative">
-                <div class="card-cover-image w-full absolute grid grid-rows-3 h-full m-4">
+        <li class="border-solid border-4 border-gray-300" id="${Skill.id}">
+            <div class="card-wrapper w-full aspect-[3/2] relative">
+                <div class="card-cover-image w-full absolute grid grid-rows-[6fr_2fr_1fr] h-full m-4">
                     <div>1</div>
                     <div>2</div>
                     <div>3</div>
                 </div>
-                <div class="card-content aspect-square">
+                <div class="card-content aspect-[3/2]">
                     <ul class="flex flex-col p-3 pt-8">
                         Banner Content
                     </ul>
