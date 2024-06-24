6f6a10ad96f9a82d4f23435dd6d5a71bfd99c556
justinscottjenecke <justinjenecke@gmail.com>
2024-06-24 13:01:08 +0200

    untested-feature: modifies template to have conditional project links

--- src/components/3-projects/projects.js
+++ src/components/3-projects/projects.js
@@ -1,25 +1,36 @@
-// ================================ Functions ===============================
+// ================================ Templating ===============================
 
-const createSkillCard = (Skill) => {
+const createProjectPreviewCard = (Project, thumbnailFilepath = "") => {
     return `               
-        <li class="border-solid border-4 border-gray-300" id="${Skill.id}">
+        <li class="border-solid border-4 border-gray-300" id="${Project.id}">
             <div class="card-wrapper w-full aspect-[4/3] relative">
                 <div class="card-cover-image w-full absolute grid grid-rows-[5fr_2fr_2fr] h-full m-4">
-                    <div>? preview-image</div>
-                    <div>? project details</div>
+                    <div>
+                        <img src="${thumbnailFilepath}${Project.thumbnail}" alt="project-preview-thumb">
+                    </div>
+                    <div>${Project.description}</div>
                     <div class="grid grid-cols-2 grid-rows-2 mb-4">
                         <button>
                             View More
                         </button>
-                        <button>
-                            Live Demo
-                        </button>
-                        <button>
-                            Repository
-                        </button>
-                        <button>
-                            Demo Video
-                        </button>
+                        ${Project.links.demo && 
+                        `
+                            <button>
+                                Live Demo
+                            </button>
+                        `}
+                        ${Project.links.repo &&  
+                        `
+                            <button>
+                                Repository
+                            </button>
+                        `}
+                        ${Project.links.preview &&  
+                        `
+                            <button>
+                                Demo Video
+                            </button>
+                        `}
                     </div>
                 </div>
                 <div class="card-content aspect-[4/3]">
@@ -32,8 +43,9 @@ const createSkillCard = (Skill) => {
     `;
 }
 
+// ============================== Functions =================================
 
-const renderSkills = (selector, skillList) => {
+const renderProjectPreviews = (selector, skillList) => {
     const entryPoint = document.querySelector(selector);
 
     skillList.forEach(skill => {
