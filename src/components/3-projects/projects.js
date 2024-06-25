e221aea55399b36f937a1ccb46aa5b9a644d3d67
justinscottjenecke <justinjenecke@gmail.com>
2024-06-25 14:52:02 +0200

    refactor: refactors conditional buttons to button object + adds correct project data to buttons

--- src/components/3-projects/projects.js
+++ src/components/3-projects/projects.js
@@ -3,8 +3,32 @@
 const createProjectPreviewCard = (thumbnailFilepath, Project) => {
 
     const buttons = {
-
+        demo: Project.links.demo ? 
+            `
+                <a href="${Project.links.demo}" target="_blank" rel="noopener noreferrer" >
+                    <button class="cursor-pointer w-full">
+                        Live Demo
+                    </button>
+                </a>
+            ` : "",
+        repo: Project.links.repo ? 
+            `
+                <a href="${Project.links.repo}" target="_blank" rel="noopener noreferrer" >
+                    <button class="cursor-pointer w-full">
+                        Repository
+                    </button>
+                </a>
+            ` : "",
+        video: Project.links.video ? 
+            `
+                <a href="${Project.links.video}" target="_blank" rel="noopener noreferrer" >
+                    <button class="cursor-pointer w-full">
+                        Demo Video
+                    </button>
+                </a>
+            ` : "" 
     }
+
     const banner = `
         <div class="project-banner">
             <div class="flex flex-col justify-center items-center p-3 pt-8">
@@ -15,9 +39,8 @@ const createProjectPreviewCard = (thumbnailFilepath, Project) => {
         </div>
     `
 
-
     const completeThumbPath = thumbnailFilepath + Project.thumbnail;
-    console.log(completeThumbPath)
+    //console.log(completeThumbPath)
 
     // 0800 030 007
     // 0800 030 007
@@ -25,41 +48,20 @@ const createProjectPreviewCard = (thumbnailFilepath, Project) => {
     return `               
         <li class="border-solid border-4 border-gray-300" id="${Project.id}">
             <div class="project-preview-wrapper w-full aspect-[4/3]">
-                <div class="project-preview-card w-full grid grid-rows-[5fr_2fr_2fr] h-full m-4">
+                <div class="project-preview-card w-full grid grid-rows-[5fr_2fr_1fr] h-full m-4">
                     <div class="overflow-hidden">
                         <img src="${completeThumbPath}" alt="project-preview-thumb" class="pr-8">
                     </div>
                     <div class="pr-4 mt-2 overflow-hidden">${Project.description}</div>
-                    <div class="grid grid-cols-2 grid-rows-2 mt-2">
+                    <div class="grid grid-cols-2 grid-rows-2 mb-2 mt-3">
                         <a rel="noopener noreferrer">
-                            <button  class="cursor-pointer">
+                            <button  class="cursor-pointer w-full">
                                 View More
                             </button>
                         </a>
-                        ${Project.links.demo &&
-                        `
-                            <a href="${Project.links.repo}" target="_blank" rel="noopener noreferrer">
-                                <button class="cursor-pointer">
-                                    Live Demo
-                                </button>
-                            </a>
-                        `}
-                        ${Project.links.repo &&
-                        `       
-                            <a href="${Project.links.repo}" target="_blank" rel="noopener noreferrer">
-                                <button  class="hover:cursor-pointer">
-                                    Repository
-                                </button>
-                            </a>
-                        `}
-                        ${Project.links.preview &&
-                        `
-                            <a href="${Project.links.repo}" target="_blank" rel="noopener noreferrer">
-                                <button class="hover:cursor-pointer">
-                                    Demo Video
-                                </button>
-                            </a>
-                        `}
+                        ${buttons.demo}
+                        ${buttons.repo}
+                        ${buttons.video}
                     </div>
                 </div>
             </div>
