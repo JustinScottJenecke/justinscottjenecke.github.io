ec9e3ca9190cb5eb419c06e6b44aedf38e5a8fc5
justinscottjenecke <justinjenecke@gmail.com>
2024-06-25 11:46:23 +0200

    bug: janky links, layout, and banner on project-preview-cards + removes banner from project-preview-cards

--- src/components/3-projects/projects.js
+++ src/components/3-projects/projects.js
@@ -5,6 +5,15 @@ const createProjectPreviewCard = (thumbnailFilepath, Project) => {
     const buttons = {
 
     }
+    const banner = `
+        <div class="project-banner">
+            <div class="flex flex-col justify-center items-center p-3 pt-8">
+                <h3 class="bg-slate-900">
+                    Effect Content
+                </h3>
+            </div>
+        </div>
+    `
 
 
     const completeThumbPath = thumbnailFilepath + Project.thumbnail;
@@ -15,41 +24,39 @@ const createProjectPreviewCard = (thumbnailFilepath, Project) => {
 
     return `               
         <li class="border-solid border-4 border-gray-300" id="${Project.id}">
-            <div class="card-wrapper w-full aspect-[4/3] relative">
-                <div class="card-cover-image w-full absolute grid grid-rows-[5fr_2fr_2fr] h-full m-4">
-                    <div class="overflow-hidden ">
+            <div class="project-preview-wrapper w-full aspect-[4/3] relative">
+                <div class="project-preview-card w-full absolute grid grid-rows-[5fr_2fr_2fr] h-full m-4">
+                    <div class="overflow-hidden">
                         <img src="${completeThumbPath}" alt="project-preview-thumb" class="pr-8">
                     </div>
-                    <div class="pr-4 mt-2">${Project.description}</div>
+                    <div class="pr-4 mt-2 overflow-hidden">${Project.description}</div>
                     <div class="grid grid-cols-2 grid-rows-2 mb-4">
-                        <button>
+                        <button  class="cursor-pointer">
                             View More
                         </button>
-                        ${Project.links.demo && 
-                        `
-                            <button>
+                        ${Project.links.demo &&
+        `
+                            <button  class="cursor-pointer">
                                 Live Demo
                             </button>
                         `}
-                        ${Project.links.repo &&  
-                        `
-                            <button>
-                                Repository
-                            </button>
+                        ${Project.links.repo &&
+                        `       
+                            <a href="${Project.links.repo}" target="_blank" rel="noopener noreferrer">
+                                <button  class="hover:cursor-pointer">
+                                    Repository
+                                </button>
+                            </a>
                         `}
-                        ${Project.links.preview &&  
+                        ${Project.links.preview &&
                         `
-                            <button>
+                            <button class="hover:cursor-pointer">
                                 Demo Video
                             </button>
                         `}
                     </div>
                 </div>
-                <div class="card-content aspect-[4/3]">
-                    <ul class="flex flex-col p-3 pt-8">
-                        Effect Content
-                    </ul>
-                </div>
+
             </div>
         </li>
     `;
