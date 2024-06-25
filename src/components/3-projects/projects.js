67cef15f9f854f05a3627be700c966ef8dbb60cc
justinscottjenecke <justinjenecke@gmail.com>
2024-06-25 09:46:15 +0200

    feature: fixes issue with project preview thumbnails not showing and adds layout styling to remove content overflow

--- src/components/3-projects/projects.js
+++ src/components/3-projects/projects.js
@@ -1,14 +1,26 @@
 // ================================ Templating ===============================
 
-const createProjectPreviewCard = (Project, thumbnailFilepath = "") => {
+const createProjectPreviewCard = (thumbnailFilepath, Project) => {
+
+    const buttons = {
+
+    }
+
+
+    const completeThumbPath = thumbnailFilepath + Project.thumbnail;
+    console.log(completeThumbPath)
+
+    // 0800 030 007
+    // 0800 030 007
+
     return `               
         <li class="border-solid border-4 border-gray-300" id="${Project.id}">
             <div class="card-wrapper w-full aspect-[4/3] relative">
                 <div class="card-cover-image w-full absolute grid grid-rows-[5fr_2fr_2fr] h-full m-4">
-                    <div>
-                        <img src="${thumbnailFilepath}${Project.thumbnail}" alt="project-preview-thumb">
+                    <div class="overflow-hidden ">
+                        <img src="${completeThumbPath}" alt="project-preview-thumb" class="pr-8">
                     </div>
-                    <div>${Project.description}</div>
+                    <div class="pr-4 mt-2">${Project.description}</div>
                     <div class="grid grid-cols-2 grid-rows-2 mb-4">
                         <button>
                             View More
@@ -49,16 +61,8 @@ const renderProjectPreviews = (selector, thumbnailFilepath, projectList) => {
     const entryPoint = document.querySelector(selector);
 
     projectList.forEach(project => {
-        entryPoint.innerHTML += createProjectPreviewCard(project)
+        entryPoint.innerHTML += createProjectPreviewCard(thumbnailFilepath, project)
     });
 }
 
 // ================================ Listeners ===============================
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    // const entryPoint = document.querySelector('skills-container');
-
-    fetchAndParseJSON("./src/data/projects.json")
-        .then(projectData => renderProjectPreviews('#all-projects-container', "../../resrource/projects/", projectData))
-})
