878e5628702d23c814b1b21d23ec1ec95b0f0f4c
justinscottjenecke <justinjenecke@gmail.com>
2024-07-08 15:20:23 +0200

    feature: adds controls for opening and closing mobile nav on burget btn click

--- src/components/1-nav/nav.js
+++ src/components/1-nav/nav.js
@@ -6,9 +6,15 @@
 
 const mobileNav = {
     burgerBtn : document.querySelector("#mobile-nav-logo-btn"),
-    linksContainer : document.querySelector(".mobile-nav-links")
+    linksContainer : document.querySelector(".mobile-nav-links"),
+    openNav: false
 };
 
+// toggles mobile nav open or closed
 mobileNav.burgerBtn.addEventListener('click', (e) => {
     
+    mobileNav.openNav = !mobileNav.openNav;
+    console.log(mobileNav.openNav)
+
+    mobileNav.openNav ? mobileNav.linksContainer.classList.add("mobile-links-active") : mobileNav.linksContainer.classList.remove("mobile-links-active") 
 })
