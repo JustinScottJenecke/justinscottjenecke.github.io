9b7afc84dee6ec1414bb5c399ff12c5cc871a346
justinscottjenecke <justinjenecke@gmail.com>
2024-07-08 15:06:25 +0200

    refactor: improves naming conventions of mobile nav elements

---
+++ src/components/1-nav/nav.js
@@ -0,0 +1,14 @@
+// -------------------------------------- Standard Nav ------------------------------------
+
+
+
+// -------------------------------------- Responsive Nav ------------------------------------
+
+const mobileNav = {
+    burgerBtn : document.querySelector("#mobile-nav-logo-btn"),
+    linksContainer : document.querySelector(".mobile-nav-links")
+};
+
+mobileNav.burgerBtn.addEventListener('click', (e) => {
+    
+})
