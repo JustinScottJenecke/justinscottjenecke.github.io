0c8dcf626e2a18d6e3c6ab473f66ed86241b1ade
justinscottjenecke <justinjenecke@gmail.com>
2024-07-12 10:13:03 +0200

    feature: creates up and down functions for filteredprojects in session storage

---
+++ src/utils/data-persistence.js
@@ -0,0 +1,23 @@
+
+const STORAGE_KEYS = {
+    MASTER_KEY : "apid-jjpw-8565",
+    featuredProjects : "-featured-projects"
+};
+
+/**
+ * Persists array of featured projects to session storage for caching purposes
+ * @param {Array} filteredProjectObjects - Array of project objects that have featured property set to true
+ * @returns {Boolean}
+ */
+const presistFeaturedProjectd = (filteredProjectObjects) => {
+    sessionStorage.setItem(`${STORAGE_KEYS.MASTER_KEY + STORAGE_KEYS.featuredProjects}`, JSON.stringify(filteredProjectObjects));
+    return true;
+};
+
+/**
+ * Retrieves array of featured projects to be used in featured-projects-component or returns null 
+ * @returns {Array|null}
+ */
+const retrieveFeatureProjects = () => {
+    return sessionStorage.getItem((`${STORAGE_KEYS.MASTER_KEY + STORAGE_KEYS.featuredProjects}`) );
+};
